efd95da1fcd2c159270989685b1bb8887cac7ebe
lukas <82029186+42lukas@users.noreply.github.com>
2025-09-25 15:15:16 +0200

    added live enemy counter and second button for second door

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,9 +1,50 @@
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Animator))]
 public class DoorOpenOnPress : MonoBehaviour
 {
+    [SerializeField] private EnemySpawner spawner;
+    [SerializeField] private TextMeshPro text;
     Animator anim;
-    void Awake() => anim = GetComponent<Animator>();
-    public void Open() => anim.SetTrigger("OpenDoor");
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Open()
+    {
+        if (spawner.CurrentAlive < 0)
+        {
+            anim.SetTrigger("OpenDoor");
+            text.color = Color.green;
+            text.text = "Access Granted!";
+        } else
+        {
+            text.color = Color.red;
+            GetComponent<TextFlash>().ShowTemporaryMessage("Access Denied!", 3f);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (spawner != null)
+        {
+            // Initial auslesen
+            UpdateUI(spawner.CurrentAlive);
+            // Live-Updates
+            spawner.OnAliveChanged += UpdateUI;
+        }
+    }
+    void OnDisable()
+    {
+        if (spawner != null) spawner.OnAliveChanged -= UpdateUI;
+    }
+
+    void UpdateUI(int alive)
+    {
+        text.color = Color.white;
+        text.text = $"Current Enemies: {alive}";
+    }
 }
